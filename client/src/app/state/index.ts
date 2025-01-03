2e4a034d956f772f0078688601c138ceaa2ffb7f
satishamara17 <kumar17.amara@gmail.com>
2025-01-03 16:36:36 -0500

    pushin sidebar and navbar

---
+++ client/src/app/state/index.ts
@@ -0,0 +1,25 @@
+import {createSlice , isDraft, PayloadAction} from "@reduxjs/toolkit"
+
+export interface InitialStateTypes {
+    isSidebarCollapsed: boolean;
+    isDarkMode: boolean;
+}
+
+const initialState: InitialStateTypes ={
+    isSidebarCollapsed: false,
+    isDarkMode: false,
+}
+
+export const globalSlice = createSlice({
+    name:'global',
+    initialState,
+    reducers: {
+        setIsSidebarCollapsed : (state, action: PayloadAction <boolean>) => {
+            state.isSidebarCollapsed = action.payload;
+        },
+        setIsDarkMode: (state, action: PayloadAction <boolean>) => {
+            state.isDarkMode = action.payload;
+        },
+    },
+});
+
