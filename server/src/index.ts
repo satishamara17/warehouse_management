1ed10feedd120d39c0d371016ccba211b0ec7424
satishamara17 <kumar17.amara@gmail.com>
2025-01-08 19:09:53 -0500

    stable

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,30 +1,34 @@
-import express from "express"
+import express from "express";
 import dotenv from "dotenv";
-import bodyParser  from "body-parser";
+import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+/* ROUTE IMPORTS */
 import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRoutes";
 import userRoutes from "./routes/userRoutes";
 import expenseRoutes from "./routes/expenseRoutes";
 
+/* CONFIGURATIONS */
 dotenv.config();
 const app = express();
-app.use (express.json());
+app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use("/dashboard", dashboardRoutes); // http://loacalhost:8000/dashboard
-app.use("/products", productRoutes);   // http://loacalhost:8000/products
-app.use("/users", userRoutes); //http://localhost:8000/users
+/* ROUTES */
+app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
+app.use("/products", productRoutes); // http://localhost:8000/products
+app.use("/users", userRoutes); // http://localhost:8000/users
 app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
 
+/* SERVER */
 const port = Number(process.env.PORT) || 3001;
-app.listen(port, "0.0.0.0" ,() =>{
-    console.log('server running on port ', port);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
